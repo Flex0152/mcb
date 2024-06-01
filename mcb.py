0d8ea5c9f137f04686c4d94dec2e022336f72331
felix <flex89@web.de>
2024-06-01 16:16:30 +0200

    add typer to mcb

--- mcb.py
+++ mcb.py
@@ -6,14 +6,24 @@ import sqlalchemy.orm as so
 from model import Content
 from app import engine
 import pandas as pd
+from rich.panel import Panel
+from rich import print
+from typer import Typer
+
+
+app = Typer()
 
 
 def get_all_rows():
+    """Hilfsfunktion für convert_to_DataFrame.
+    Ruft alle Zeilen der Content Tabelle ab."""
     with so.Session(engine) as session:
         result = session.scalars(sa.Select(Content)).all()
     return result
 
-def convert_to_DataFrame():
+def convert_to_DataFrame() -> pd.DataFrame:
+    """Erstellt im ersten Schritt eine Liste mit Dictonarys. Diese Liste 
+    wird in ein DataFrame konvertiert und zurückgegeben"""
     erg = []
     results = get_all_rows()
     for row in results:
@@ -24,12 +34,15 @@ def convert_to_DataFrame():
         erg.append(tmp)
     return pd.DataFrame(erg)
 
+@app.command()
 def load_content_by_id(id: int):
+    """Ruft den Content der angegebenen ID ab. Gibt den gefundenen 
+    Content über Std Out in einem rich Panel aus"""
     with so.Session(engine) as session:
         stmt = sa.Select(Content).where(Content.id == id)
         result = session.scalar(stmt)
-    print(result.content)
+    print(Panel(result.content))
 
 
 if __name__ == '__main__':
-    print(convert_to_DataFrame())
+    app()
