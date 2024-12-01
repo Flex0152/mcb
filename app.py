8b510e56bc200dd930a9ae1c3ff786f21bd8ee19
Felix Weidemann <felix>
2024-12-01 09:43:12 +0100

    final version 1

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from sqlalchemy.orm import Session
 import sqlalchemy as sa
 from model import engine
 
+from icecream import ic
+
 
 def create_window(window_name, session_name):
     stmt = sa.Select(Window).where(Window.windowName == window_name[-1].strip())
@@ -21,28 +23,37 @@ def add_content(txt, window, session_name):
     session_name.add(window)
     session_name.commit()
 
+def get_clipboard() -> str | None:
+    try:
+        return pyperclip.paste()
+    except pyperclip.PyperclipException:
+        return None
+
+def format_output(value: str) -> None:
+    print("+"*40)
+    print()
+    print(value + "\n")
+
 
 def main():
+    # initial
+    old_value = get_clipboard()
     try:
         with Session(engine) as session:
             while True:
-                
-                try:
-                    sleep(0.5)
-                    rprint(pyperclip.paste())
-                    print()
-                    txt = pyperclip.waitForNewPaste()
-                    if txt == "":
-                        txt = "Inhalt nicht zu erfassen!"
-                # Wird das Notebook gesperrt, ist kein Zugriff auf die Zwischenablage möglich. -> Ausnahme wird abgefangen und ignoriert.
-                except pyperclip.PyperclipWindowsException:
-                    continue
-
-                window_name = str(gw.getActiveWindow().title).split("-")
-                window = create_window(window_name, session)
-                
-                add_content(txt, window, session)
-
+                sleep(1)
+                # get clipboard every second
+                new_value = get_clipboard()
+                # if the new value not equal to the old value
+                # and if new value not None
+                if old_value != new_value and new_value != None:
+                    format_output(new_value)
+                    window_name = str(gw.getActiveWindow().title).split("-")
+                    window = create_window(window_name, session)
+                    # add the content to database
+                    add_content(new_value, window, session)
+                    # assign the new value to the old_value variable        
+                    old_value = new_value
     except KeyboardInterrupt:
         rprint("[*] Programm beendet!")
     except UnicodeDecodeError as e:
